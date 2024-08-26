f4abd7fb11ebceb589728330e596f10edd79374f
Leo Škvorc <leo.skvorc@montelektro.hr>
2024-08-26 19:55:20 +0200

    added logging

---
+++ InstagramCopy/server/Domain/Migrations/20240826112625_logging.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InstagramCopy.Migrations
+{
+    /// <inheritdoc />
+    public partial class logging : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "App");
+
+            migrationBuilder.CreateTable(
+                name: "InstagramLog",
+                schema: "App",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OccurredAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Operation = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RequestQuery = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RequestBody = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InstagramLog", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InstagramLog",
+                schema: "App");
+        }
+    }
+}
---
+++ InstagramCopy/server/InstagramCopy/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using Domain.Data.Models.DbModels;
+using InstagramCopy.Data;
+using System.Security.Claims;
+using System.Text;
+
+namespace InstagramCopy.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, ApplicationDbContext dbContext)
+        {
+            var username = context.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name);
+
+            using var bodyReader = new StreamReader(
+                context.Request.Body,
+                Encoding.UTF8,
+                detectEncodingFromByteOrderMarks: false,
+                bufferSize: 1024,
+                leaveOpen: true);
+            var requestBody = await bodyReader.ReadToEndAsync();
+
+            var queryParams = new StringBuilder();
+
+            foreach (var param in context.Request.Query)
+            {
+                queryParams.Append($"{param.Key}={param.Value}&");
+            }
+
+            if (queryParams.Length > 0)
+            {
+                queryParams.Length--;
+            }
+
+            var instagramLog = new InstagramLog()
+            {
+                UserName = username?.Value,
+                OccurredAt = DateTime.UtcNow,
+                Operation = context.Request.Path,
+                RequestQuery = queryParams.ToString(),
+                RequestBody = requestBody,
+            };
+
+            dbContext.InstagramLogs.Add(instagramLog);
+            await dbContext.SaveChangesAsync();
+
+            await _next(context);
+        }
+    }
+
+    public static class LoggingMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseInstagramLogging(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+}
+
